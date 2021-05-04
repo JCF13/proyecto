e33fa6224c447547bb11a9b768897e791c1bd538
Jero <jerocf13@gmail.com>
2021-05-04 16:36:12 +0200

    Initial commit

---
+++ frontend/app/src/components/ModalPost.vue
@@ -0,0 +1,205 @@
+<template>
+    <q-dialog v-model="alert" persistent>
+        <q-card id="my-card">
+            <q-card-section horizontal>
+                <div id="card-left">
+                    <q-icon id="close" name="highlight_off" size="30px" @click="close"/>
+
+                    <img id="image" src="https://www.hola.com/imagenes/viajes/20180530124901/naturaleza-destinos-mundo-a-todo-color/0-571-947/colores-m.jpg" alt="">
+                
+                    <div id="pie-de-foto" class="absolute-bottom text-subtitle2">
+                        Pie de foto 
+                    </div>
+                </div>
+
+                <q-card-actions vertical id="card-right">
+                    <div id="modal-user">
+                        <q-avatar size="30px" class="avatar" color="orange">J</q-avatar>
+                        <h6>Usuario</h6>
+                    </div>
+                    <div id="modal-info">
+                        <div>
+                            <p>34</p><q-icon size="15px" name="favorite" color="red" /> 
+                            <q-icon name="favorite" style="display:none;" />
+                        </div>
+                        <div>
+                            <p>4</p>
+                            <q-icon name="comment" />
+                        </div>
+                    </div>
+                    <div id="creation-date">
+                        <p>14-3-2021</p>
+                    </div>
+                    <div id="comments">
+                        <div>
+                            <q-avatar size="20px" class="avatar" color="blue">J</q-avatar>
+                            <p><span>Usuario1.</span> Comentario 1</p>
+                        </div>
+                        <div>
+                            <q-avatar size="20px" class="avatar" color="orange">J</q-avatar>
+                            <p><span>Usuario2.</span> Comentario 2</p>
+                        </div>
+                        <div>
+                            <q-avatar size="20px" class="avatar" color="red">J</q-avatar>
+                            <p><span>Usuario3.</span> Comentario 3</p>
+                        </div>
+                        <div>
+                            <q-avatar size="20px" class="avatar" color="green">J</q-avatar>
+                            <p><span>Usuario4.</span> Comentario 4</p>
+                        </div>
+                        <div>
+                            <q-avatar size="20px" class="avatar" color="pink">J</q-avatar>
+                            <p><span>Usuario5.</span> Comentario 5</p>
+                        </div>
+                        <div>
+                            <q-avatar size="20px" class="avatar" color="purple">J</q-avatar>
+                            <p><span>Usuario6.</span> Comentario 6</p>
+                        </div>
+                        <div>
+                            <q-avatar size="20px" class="avatar" color="yellow">J</q-avatar>
+                            <p><span>Usuario7.</span> Comentario 7</p>
+                        </div>
+                        <div>
+                            <q-avatar size="20px" class="avatar" color="orange">J</q-avatar>
+                            <p><span>Usuario8.</span> Comentario 8</p>
+                        </div>
+                        <q-icon name="add_circle_outline" size="25px"/>
+                    </div>
+                    <div id="new-comment">
+                        <q-input label="Escribe un comentario" color="black">
+                            <template v-slot:append>
+                                <q-icon name="send" color="black"/>
+                            </template>
+                        </q-input>
+                    </div>
+                </q-card-actions>
+            </q-card-section>
+        </q-card>
+    </q-dialog>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            alert: true
+        }
+    },
+    methods: {
+        close() {
+            this.$router.go(-1)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    #my-card {
+        width: 100%;
+        max-width: 800px;
+    }
+
+    #image {
+        justify-self: center;
+        max-width: 400px;
+        max-height: 500px;
+        align-self: center;
+    }
+
+    #card-left {
+        display: grid;
+        width: 50%;
+        height: 500px;
+        border-right: 2px solid grey;
+        margin: 0 auto;
+    }
+
+    #card-right {
+        padding: 1%;
+        width: 50%;
+        display: grid;
+        grid-template-columns: 50% 50%;
+        grid-template-rows: 10% 10% 70% 10%;
+        grid-template-areas: 
+            "user info"
+            "fecha fecha"
+            "comments comments"
+            "comment comment";
+    }
+
+    #modal-user {
+        grid-area: user;
+        display: grid;
+        grid-template-columns: 20% 80%;
+    }
+
+    #modal-info {
+        padding-right: 5%;
+        grid-area: info;
+        display: grid;
+        grid-template-rows: 50% 50%;
+        justify-items: right;
+    }
+
+    #modal-info div:first-child {
+        display: grid;
+        grid-template-columns: 90% 10%;
+    }
+
+    #modal-info div:last-child {
+        display: grid;
+        grid-template-columns: 90% 10%;
+    }
+
+    #creation-date {
+        grid-area: fecha;
+    }
+
+    span {
+        font-weight: bold;
+    }
+
+    #close {
+        position: absolute;
+        margin: 1%;
+    }
+
+    #close {
+        cursor: pointer;
+    }
+
+    #comments {
+        grid-area: comments;
+        display: grid;
+    }
+
+    #comments div {
+        display: grid;
+        grid-template-columns: 10% 90%;
+        margin-bottom: 5%;
+    }
+
+    #comments i:last-child {
+        justify-self: center;
+    }
+
+    #pie-de-foto {
+        background: rgba(0, 0, 0, 0.692);
+        width: 50%;
+        color: white;
+        text-align: left;
+        padding: 1%;
+        height: 7%;
+    }
+
+    #new-comment {
+        grid-area: comment;
+    }
+
+</style>
---
+++ frontend/app/src/components/Profile.vue
@@ -0,0 +1,169 @@
+<template>
+    <q-page class="flex flex-center">
+        <div id="main">
+            <div id="top">
+                <div id="avatar">
+                </div>
+                <div id="usuario">
+                    <p>USUARIO</p>
+                </div>
+                <div>
+                    <q-list id="info-top">
+                        <router-link to="/inside/social/followers">
+                            <q-item clickable v-ripple id="seguidores">
+                                <q-item-section>SEGUIDORES</q-item-section>
+                            </q-item>
+                        </router-link>
+                        <router-link to="/inside/social/following">
+                            <q-item clickable v-ripple id="seguidos">
+                                <q-item-section>SEGUIDOS</q-item-section>
+                            </q-item>
+                        </router-link>
+                        <q-item>
+                            <router-link to="/inside/settings">
+                                <q-btn flat round color="black" icon="settings" />
+                            </router-link>
+                        </q-item>
+                    </q-list>
+                </div>
+            </div>
+            <div id="publicaciones">
+                <div v-for="image in images" :key="image.img">
+                    <img class="img-post" :src="image.img" alt=""
+                    @click="openPost">
+                </div>
+                
+                <div id="more">
+                    <q-icon name="add_circle_outline" size="30px"/>
+                </div>
+            </div>
+
+            
+                
+        </div>
+        <router-view/>
+    </q-page>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            images: [
+                {
+                    "img": "https://www.hola.com/imagenes/viajes/20180530124901/naturaleza-destinos-mundo-a-todo-color/0-571-947/colores-m.jpg"
+                },
+                {
+                    "img": "https://ugc.kn3.net/i/760x/http://wackymania.com/image/2011/6/vertical-panoramic-photography/vertical-panoramic-photography-06.jpg"
+                },
+                {
+                    "img": "https://i.pinimg.com/originals/c2/88/c7/c288c7ff9eae9c9f7397115b140fb2b5.jpg"
+                },
+                {
+                    "img": "https://www.hola.com/imagenes/viajes/20180530124901/naturaleza-destinos-mundo-a-todo-color/0-571-947/colores-m.jpg"
+                },
+                {
+                    "img": "https://www.hola.com/imagenes/viajes/20180530124901/naturaleza-destinos-mundo-a-todo-color/0-571-947/colores-m.jpg"
+                },
+                {
+                    "img": "https://www.hola.com/imagenes/viajes/20180530124901/naturaleza-destinos-mundo-a-todo-color/0-571-947/colores-m.jpg"
+                },
+                {
+                    "img": "https://www.hola.com/imagenes/viajes/20180530124901/naturaleza-destinos-mundo-a-todo-color/0-571-947/colores-m.jpg"
+                },
+            ],
+        }
+    },
+    methods: {
+        openPost() {
+            this.$router.push('/inside/profile/post')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    #main {
+        background: white;
+        height: 90vh;
+        width: 60%;
+        box-shadow: 0 0 10px rgb(214, 214, 214);
+        display: grid;
+        grid-template-rows: 30% 70%;
+        border-radius: 5px;
+    }
+    
+    a {
+        text-decoration: none;
+        color: black;
+    }
+
+    #top {
+        display: grid;
+        width: 100%;
+        height: 100%;
+        grid-template-columns: 100%;
+        grid-template-rows: 50% 25% 25%;
+        border-bottom: 2px solid rgb(136, 136, 136);
+    }
+
+    #avatar {
+        height: 100%;
+        width: 12%;
+        border-radius: 50%;
+        margin: 2% auto;
+        border: 2px solid rgb(0, 0, 0);
+        background-image: url("https://ugc.kn3.net/i/760x/http://wackymania.com/image/2011/6/vertical-panoramic-photography/vertical-panoramic-photography-06.jpg");
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;        
+    }
+
+    #info-top {
+        height: 100%;
+        width: 100%;
+        text-align: center;
+        font-weight: bold;
+        margin: 0 auto;
+        display: grid;
+        grid-template-columns: 47.5% 47.5% 5%;
+        justify-content: space-between;
+    }
+
+    #usuario {
+        height: 100%;
+        margin: 0 auto;
+        margin-top: 3%;
+        font-weight: bold;
+    }
+
+    #publicaciones::-webkit-scrollbar {
+        display: none;
+    }
+
+    #publicaciones {
+        overflow-y: scroll;
+        display: grid;
+        grid-template-columns: 33.33% 33.33% 33.33%;
+        grid-auto-rows: 60%;
+    }
+
+    .img-post {
+        float: left;
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+
+    #more {
+        justify-self: center;
+        align-self: center;
+    }
+
+</style>
