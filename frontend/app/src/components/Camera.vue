010dbaa18aa2b5d1a42f65e3ed839d9746ee579c
Jero <jerocf13@gmail.com>
2021-05-19 16:32:21 +0200

    Cambio en la cámara

--- frontend/app/src/components/Camera.vue
+++ frontend/app/src/components/Camera.vue
@@ -3,10 +3,11 @@
         <video class="align-middle">
             <source type="video/mp4">
         </video>
-        <q-btn push label="HACER FOTO" color="black" @click="captionDialog = true" />
+        <q-btn push label="HACER FOTO" color="black" @click="nextDialog" />
 
         <q-dialog v-model="captionDialog">
             <q-card style="width: 700px; max-width: 80vw;">
+                <img id="photo" src="" alt="">
                 <q-card-section>
                     <div class="text-h6">Añade un pie de foto si lo ves necesario</div>
                 </q-card-section>
@@ -16,7 +17,7 @@
                 </q-card-section>
 
                 <q-card-actions align="right" class="bg-white">
-                    <q-btn flat label="SIGUIENTE" @click="goToPost" />
+                    <q-btn flat label="SIGUIENTE" @click="createPost" />
                 </q-card-actions>
             </q-card>
         </q-dialog>
@@ -29,7 +30,7 @@ export default {
         return {
             mediaRecorder: null,
             captionDialog: false,
-            caption: ''
+            caption: '',
         }
     },
     created() {
@@ -51,6 +52,42 @@ export default {
         })
     },
     methods: {
+        nextDialog() {
+            let canvas = document.createElement('CANVAS');
+            let video = document.querySelector('video');
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+
+            let ctx = canvas.getContext('2d');
+            ctx.drawImage(video, 0, 0);
+
+            ctx.canvas.toBlob(async function(blob) {
+                let url = URL.createObjectURL(blob);
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    let fileInBase64 = btoa(e.target.result);
+                }
+
+                reader.readAsBinaryString(blob);
+                document.querySelector('#photo').src = url;
+            })
+
+            this.captionDialog = true;
+        },
+        async createPost() {
+            const authorization = localStorage.getItem('access_token');
+            const postFetch = await fetch('http://localhost:5000/post/cpost', {
+                method: 'POST',
+                headers: {
+                    'Authorization': JSON.stringify(authorization)
+                },
+                body: JSON.stringify({
+                    caption: this.caption,
+                    path: 'C:\\Users\\JCFJe\\Documentos\\Segundo_curso\\FCP\\proyecto\\backend\\flask_app\\app\\static\\img',
+                    fname: 'primerafoto'
+                })
+            })
+        }
     }
 }
 </script>
@@ -72,5 +109,11 @@ export default {
     button {
         width: 25%;
         font-weight: bold;
+    }
+
+    #photo {
+        width: 100%;
+        height: 100%;
+        margin: 0 auto;
     }
 </style>
