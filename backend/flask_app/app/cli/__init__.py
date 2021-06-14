b5eb92c9a94794e495c78a884ccabd63bfae454d
Toni <tjimenezsblanca@gmail.com>
2021-06-14 21:41:01 +0200

    babel eliminado y modificado logger

--- backend/flask_app/app/cli/__init__.py
+++ backend/flask_app/app/cli/__init__.py
@@ -1,17 +1,6 @@
 from flask import Blueprint
-<<<<<<< HEAD
-from flask_app.app.cli.generate import create_admin, create_db
-# from flask_app.app.cli.babel import extract,init,update,compile
-=======
 from backend.flask_app.app.cli.generate import create_admin, create_db
-# from backend.flask_app.app.cli.babel import extract,init,update,compile
->>>>>>> pruebas2
+
 gen = Blueprint('gen',__name__,cli_group='generate')
 gen.cli.add_command(create_db)
 gen.cli.add_command(create_admin)
-
-# translate = Blueprint('trans',__name__,cli_group='babel')
-# translate.cli.add_command(extract)
-# translate.cli.add_command(init)
-# translate.cli.add_command(compile)
-# translate.cli.add_command(update)
--- backend/flask_app/app/services/logs/refactor_dict.py
+++ backend/flask_app/app/services/logs/refactor_dict.py
@@ -19,10 +19,10 @@ def gen_log(myDict, level):
 
     newDict = del_null_values(myDict)
 
-    if level <= 20:
-        return gen_simple_log(newDict)
-    elif level > 20:
-        return gen_complete_log(newDict)
+    # if level <= 20:
+    return gen_simple_log(newDict)
+    # elif level > 20:
+        # return gen_complete_log(newDict)
 
 
 def gen_simple_log(myDict):
@@ -33,22 +33,21 @@ def gen_simple_log(myDict):
                 'cantidad', 'caption', 
                 'user', 'follows', 'error_type', 'error_desc',
                 'post_id']
+    _logged_in = ''
     
     for key, value in myDict.items():
         for attr_log in my_values:
             if key == attr_log:
                 if value != '':
-                    
-                    # if key == 'listart':
-                    #     if value != None:
-                    #         theLog = theLog + key + ': ' + str(len(value)) + ' | '                        
-                    # elif key == 'error':
-                    #     pass
-                    #     # for keyErr, valErr in value:
-                    #     #     theLog = theLog + keyErr + ': ' + str(valErr) + ' | '                        
-                    # elif key == 'your_auth':
-                    #     pass
-                    # else:
+                    if key == 'username':
+                        _logged_in = value
+
+                    elif key == 'access_token':
+                        if len(value) > 64:
+                            theLog = theLog + 'logged in ' + str(_logged_in) + ' | '
+                        else:
+                            theLog = theLog + 'No se ha conectado | '
+
                     theLog = theLog + key + ': ' + str(value) + ' | '
     return theLog
 
@@ -56,27 +55,14 @@ def gen_simple_log(myDict):
 def gen_complete_log(myDict):
     theLog = ''
 
-    my_values = ['request','result','your_auth','listart','empid','artid','artdes','dptid','famid','grpid','lpreid','tipart','error_type','error_desc']
+    my_values = ['username', 'email', 'user', 'request',
+                'cantidad', 'caption', 
+                'user', 'follows', 'error_type', 'error_desc',
+                'post_id']
 
     for key, value in myDict.items():
         for attr_log in my_values:
             if key == attr_log:
                 if value != '':
-                    if key == 'listart':
-                        theLog = theLog + key + ': ' + str(len(value)) + ' | '
-                        for article in value:
-                            for keyArt, attr in article.items():
-                                if keyArt == 'precios':                        
-                                    for price in attr:
-                                        for keyPrice, valPrice in price.items():
-                                            theLog = theLog + keyPrice + ': ' + str(valPrice) + ' | '
-                                else:
-                                    theLog = theLog + keyArt + ': ' + str(attr) + ' | '
-                    elif key == 'error':
-                        for keyErr, valErr in value:
-                            theLog = theLog + keyErr + ': ' + str(valErr) + ' | '                        
-                    elif key == 'your_auth':
-                        pass
-                    else:
-                        theLog = theLog + key + ': ' + str(value) + ' | '
+                    theLog = theLog + key + ': ' + str(value) + ' | '
     return theLog
